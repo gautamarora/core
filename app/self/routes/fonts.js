563f541ba55a2ddf330ef731e938dde575843257
Gautam Arora <gautamsarora3@gmail.com>
2016-01-22 17:27:55 -0500

    Fix to simplify bootstrap font loading

--- app/self/routes/fonts.js
+++ app/self/routes/fonts.js
@@ -1,7 +1,6 @@
 var path = require('path');
 
 module.exports = function(app, express) {
-  //note: if linking bootstrap from node_modules, remove the last 'bootstrap' from the path
-  var normalizedPath = path.join(__dirname, '..', '..', '..', 'node_modules', 'bootstrap-sass', 'assets', 'fonts', 'bootstrap');
+  var normalizedPath = path.join(__dirname, '..', '..', '..', 'styles', 'bootstrap', 'fonts');
   app.use('/fonts', express.static(normalizedPath));
 };
