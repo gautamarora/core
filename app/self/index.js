f00a96c489cefcf6c6ff75262de53637331c5d06
Gautam Arora <gautamsarora3@gmail.com>
2016-02-09 00:16:48 -0500

    Update to add self init log

--- app/self/index.js
+++ app/self/index.js
@@ -1,6 +1,7 @@
 var core = require('../index');
 
 module.exports.init = function (app, express) {
+  console.log('core self init');
   core.registerRoutes(__dirname, 'routes', app, express);
   core.registerPartials(__dirname, 'partials', 'core');
 };
